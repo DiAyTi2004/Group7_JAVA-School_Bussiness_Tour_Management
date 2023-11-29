b79d58a69ff5f792c2ab076b2592433874bec797
Dinh Tuan Dat <dtuandat2004@gmail.com>
2023-11-30 02:27:01 +0700

    initialize project

---
+++ src/group7_java/school_bussiness_tour_management/Group7_JAVASchool_Bussiness_Tour_Management.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package group7_java.school_bussiness_tour_management;
+
+import group7_java.school_bussiness_tour_management.views.Login;
+
+/**
+ *
+ * @author PC
+ */
+public class Group7_JAVASchool_Bussiness_Tour_Management {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Login loginView = new Login();
+        loginView.setVisible(true);
+    }
+    
+}
---
+++ src/group7_java/school_bussiness_tour_management/models/Student.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package group7_java.school_bussiness_tour_management.models;
+
+/**
+ *
+ * @author PC
+ */
+public class Student {
+    private static int count;
+    private int id;
+    private String code;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String phoneNumber;
+    private int classId;
+    
+    
+}
